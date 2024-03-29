7a382891954de51fd789149020e08c29f98c14a6
Cabezon.luac <94794277+0x73616D@users.noreply.github.com>
2024-03-29 15:42:35 -0300

    Plata_Banco.lua

--- Plata_Banco.lua
+++ Plata_Banco.lua
@@ -2,5 +2,5 @@ local Script = {
     NickName = "VER DINERO EN EL BANCO",
     By = {"0x73616D.luac"},
     Code = {"LUA"},
-    Buy this script = "https://discord.com/users/717764929113030756"
+    Buy this script = "https://discord.com/users/717764929113030756" 
 }
