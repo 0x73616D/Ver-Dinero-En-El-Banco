71d0320c19ee3e8070bbeab27940cf495c484da0
Cabezon.luac <94794277+0x73616D@users.noreply.github.com>
2024-03-29 15:42:24 -0300

    Update and rename Plata_Banco to Plata_Banco.lua

--- Plata_Banco.lua
+++ Plata_Banco.lua
@@ -1,5 +1,5 @@
 local Script = {
-    NickName = "⭐ VER DINERO EN EL BANCO",
+    NickName = "VER DINERO EN EL BANCO",
     By = {"0x73616D.luac"},
     Code = {"LUA"},
     Buy this script = "https://discord.com/users/717764929113030756"
